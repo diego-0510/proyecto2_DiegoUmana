a11303f71f61b319e2f0ae4cdd96393956125971
Diego Umaña <francisco.ussan16@gmail.com>
2022-07-15 23:31:04 -0600

    foto

--- proyecto2_DiegoUmana/frmPrincipal.Designer.cs
+++ proyecto2_DiegoUmana/frmPrincipal.Designer.cs
@@ -32,14 +32,17 @@ namespace proyecto2_DiegoUmana
             this.components = new System.ComponentModel.Container();
             this.txtCedula = new System.Windows.Forms.TextBox();
             this.blCedula = new System.Windows.Forms.Label();
-            this.btnFoto = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblHora = new System.Windows.Forms.Label();
+            this.picBoxFoto = new System.Windows.Forms.PictureBox();
+            this.btnHabilitar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxFoto)).BeginInit();
             this.SuspendLayout();
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(121, 226);
+            this.txtCedula.Location = new System.Drawing.Point(153, 258);
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(180, 20);
             this.txtCedula.TabIndex = 0;
@@ -48,20 +51,22 @@ namespace proyecto2_DiegoUmana
             // 
             this.blCedula.AutoSize = true;
             this.blCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.blCedula.Location = new System.Drawing.Point(147, 188);
+            this.blCedula.Location = new System.Drawing.Point(179, 220);
             this.blCedula.Name = "blCedula";
             this.blCedula.Size = new System.Drawing.Size(144, 24);
             this.blCedula.TabIndex = 1;
             this.blCedula.Text = "Digite su cedula";
             // 
-            // btnFoto
+            // btnIngresar
             // 
-            this.btnFoto.Location = new System.Drawing.Point(171, 269);
-            this.btnFoto.Name = "btnFoto";
-            this.btnFoto.Size = new System.Drawing.Size(75, 23);
-            this.btnFoto.TabIndex = 2;
-            this.btnFoto.Text = "button1";
-            this.btnFoto.UseVisualStyleBackColor = true;
+            this.btnIngresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnIngresar.Location = new System.Drawing.Point(194, 301);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(93, 29);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // timer1
             // 
@@ -71,24 +76,47 @@ namespace proyecto2_DiegoUmana
             // 
             this.lblHora.AutoSize = true;
             this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHora.Location = new System.Drawing.Point(155, 47);
+            this.lblHora.Location = new System.Drawing.Point(187, 27);
             this.lblHora.MinimumSize = new System.Drawing.Size(100, 50);
             this.lblHora.Name = "lblHora";
             this.lblHora.Size = new System.Drawing.Size(100, 50);
             this.lblHora.TabIndex = 4;
+            this.lblHora.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // picBoxFoto
+            // 
+            this.picBoxFoto.Location = new System.Drawing.Point(166, 100);
+            this.picBoxFoto.Name = "picBoxFoto";
+            this.picBoxFoto.Size = new System.Drawing.Size(157, 98);
+            this.picBoxFoto.TabIndex = 5;
+            this.picBoxFoto.TabStop = false;
+            // 
+            // btnHabilitar
+            // 
+            this.btnHabilitar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnHabilitar.Location = new System.Drawing.Point(80, 301);
+            this.btnHabilitar.Name = "btnHabilitar";
+            this.btnHabilitar.Size = new System.Drawing.Size(93, 29);
+            this.btnHabilitar.TabIndex = 6;
+            this.btnHabilitar.Text = "habilitar";
+            this.btnHabilitar.UseVisualStyleBackColor = true;
+            this.btnHabilitar.Click += new System.EventHandler(this.btnHabilitar_Click);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 428);
+            this.ClientSize = new System.Drawing.Size(470, 428);
+            this.Controls.Add(this.btnHabilitar);
+            this.Controls.Add(this.picBoxFoto);
             this.Controls.Add(this.lblHora);
-            this.Controls.Add(this.btnFoto);
+            this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.blCedula);
             this.Controls.Add(this.txtCedula);
             this.Name = "frmPrincipal";
             this.Text = "Entradas y Salidas";
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,9 +126,11 @@ namespace proyecto2_DiegoUmana
 
         private System.Windows.Forms.TextBox txtCedula;
         private System.Windows.Forms.Label blCedula;
-        private System.Windows.Forms.Button btnFoto;
+        private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.PictureBox picBoxFoto;
+        private System.Windows.Forms.Button btnHabilitar;
     }
 }
 
--- proyecto2_DiegoUmana/frmPrincipal.cs
+++ proyecto2_DiegoUmana/frmPrincipal.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +21,63 @@ namespace proyecto2_DiegoUmana
             InitializeComponent();
         }
 
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        bool isCameraRunning=true;
+
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+
+            frame = new Mat();
+            capture = new VideoCapture(0);
+            capture.Open(0);
+
+            if (capture.IsOpened())
+            {
+                while (isCameraRunning)
+                {
+
+                    capture.Read(frame);
+                    image = BitmapConverter.ToBitmap(frame);
+                    if (picBoxFoto.Image != null)
+                    {
+                        picBoxFoto.Image.Dispose();
+                    }
+                    picBoxFoto.Image = image;
+                }
+            }
+        }
+
+        private  void btnIngresar_Click(object sender, EventArgs e)
+        {
+            CaptureCamera();
+            
+            if (isCameraRunning)
+            {
+
+                // Tome una instantánea de la imagen actual generada por OpenCV en el cuadro de imagen
+                
+                Bitmap snapshot = new Bitmap(picBoxFoto.Image);
+
+                // Guardar en algún directorio
+                // en este ejemplo, generaremos un nombre de archivo aleatorio, por ejemplo 47059681-95ed-4e95-9b50-320092a3d652.png
+                // snapshot.Save(@"C:\Users\sdkca\Desktop\mysnapshot.png", ImageFormat.Png);
+                snapshot.Save(string.Format("C://Users//franc//source//repos//proyecto2_DiegoUmana//foto.png", Guid.NewGuid()), ImageFormat.Png);
+            }
+            else
+            {
+                Console.WriteLine("¡No se pueden tomar fotografías si la cámara no está capturando imágenes!");
+            }
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             lblHora.Text = DateTime.Now.ToString("hh:mm:ss");
@@ -25,11 +86,24 @@ namespace proyecto2_DiegoUmana
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             timer1.Enabled = true;
+            
         }
-
-        private void lblHora_Click(object sender, EventArgs e)
+        
+        private void btnHabilitar_Click(object sender, EventArgs e)
         {
-
+            if (btnHabilitar.Text.Equals("habilitar"))
+            {
+                CaptureCamera();
+                btnHabilitar.Text = "Stop";
+                isCameraRunning = true;
+            }
+            else
+            {
+                capture.Release();
+                btnHabilitar.Text = "Start";
+                isCameraRunning = false;
+            }
         }
+        
     }
 }
