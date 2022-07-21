0a0a4ceee715386ee44cd12cdc44473a8362a176
diego-0510 <105898843+diego-0510@users.noreply.github.com>
2022-07-21 17:10:07 -0600

    Add files via upload

---
+++ bdproyecto2.sql
@@ -0,0 +1,12 @@
+-- Database: proyecto2
+
+-- DROP DATABASE IF EXISTS proyecto2;
+
+CREATE DATABASE proyecto2
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Latin America.1252'
+    LC_CTYPE = 'Spanish_Latin America.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
